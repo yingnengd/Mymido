750285670e45d2764ce13560282485125e72c48a
yingnengd <704441474@qq.com>
2021-12-14 11:46:24 +0800

    1 Signed-off-by: yingnengd <704441474@qq.com>

--- mido/midifiles/midifiles.py
+++ mido/midifiles/midifiles.py
@@ -305,7 +305,7 @@ class MidiFile(object):
     def __init__(self, filename=None, file=None,
                  type=1, ticks_per_beat=DEFAULT_TICKS_PER_BEAT,
                  charset='latin1',
-                 debug=True,
+                 debug=False,
                  clip=True,
                  tracks=None
                  ):
